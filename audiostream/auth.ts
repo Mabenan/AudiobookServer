f1b38267c607d691c58fdc806b3b8cfc332b19ae
Mabenan <doener123@googlemail.com>
2021-03-31 09:19:57 +0200

    Update auth.ts

--- audiostream/auth.ts
+++ audiostream/auth.ts
@@ -7,13 +7,16 @@ import { Cloud } from "parse";
 export function auth(appID : string) {
 
     return function auth(req: Request, res: Response, next: CallableFunction) {
-        if(req.method == "OPTIONS"){
+        if (req.path.includes("stream")) {
             
+            res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD');
-            next();
+            res.header('Access-Control-Allow-Headers', 'X-Parse-Master-Key, X-Parse-REST-API-Key, X-Parse-Javascript-Key, X-Parse-Application-Id, X-Parse-Client-Version, X-Parse-Session-Token, X-Requested-With, X-Parse-Revocable-Session, X-Parse-Request-Id, Content-Type, Pragma, Cache-Control, X-Parse-Installation-Id, X-Parse-Application-Id');
+            res.header('Access-Control-Expose-Headers', 'X-Parse-Job-Status-Id, X-Parse-Push-Status-Id');
+        if(req.method == "OPTIONS"){
+            res.sendStatus(200);
             return;
         }
-        if (req.path.includes("stream")) {
             if (!req.headers["x-parse-session-token"]) {
                 res.status(401).send("not authorized");
                 return;
