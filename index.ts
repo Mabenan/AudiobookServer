485a450f8d125503973f8d90320b9c026821e6c2
mabenan <doener123@googlemail.com>
2021-08-16 15:12:29 +0200

    update api

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ import * as serve from "./audiostream/serve";
 import * as auth from "./audiostream/auth"; 
 import {Album} from "./data/Album";
 import { Track } from "./data/Track";
-import { instance } from "server-manager-api";
+import { instance, InitProcess } from "server-manager-api";
 
 Parse.Object.registerSubclass("Album", Album);
 Parse.Object.registerSubclass("Track", Track);
 
-function init(){
-instance.APP.use(auth.auth("com.mabenan.catbooks"))
-    instance.APP.use("/stream", serve.serve);
+module.exports = class Init implements InitProcess{
+    init() {
+        instance.APP.use(auth.auth("com.mabenan.catbooks"))
+            instance.APP.use("/stream", serve.serve);
+    }
+
 }
-init();
